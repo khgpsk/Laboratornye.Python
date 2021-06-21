cd1e2612630c558a0042eb9de1896d7673cb0e14
khgpsk <86227559+khgpsk@users.noreply.github.com>
2021-06-21 12:39:41 +0300

    lab1
    lab1

---
+++ laba1Pytho/laba1Pytho/laba1Pytho.py
@@ -0,0 +1,10 @@
+chislo = int(input("Введите число: "))
+pogchislo = 12
+if chislo == pogchislo:
+    print("Введено пограничное число!")
+if chislo < pogchislo:
+    print("1ое число меньше пограничного")
+if chislo > pogchislo:
+    print("1ое число больше пограничного")
+if chislo > pogchislo*3:
+    print("1ое число больше пограничного более, чем в 3 раза")
