0fa4eb0e4d407e4d7d0420472fde149d799f2e92
khgpsk <86227559+khgpsk@users.noreply.github.com>
2021-06-22 11:12:23 +0300

    Update laba6.3Python.py

--- laba6.3Python/laba6.3Python/laba6.3Python.py
+++ laba6.3Python/laba6.3Python/laba6.3Python.py
@@ -31,32 +31,32 @@ def factorial(number1):
 def arccos(number1):
     return number1*math.pi/180
 
-do = input("Введите арифметическое действие, либо 0 (рандомное число): ")
-while do != "q":
-    if do == "+":
+znak = input("Введите арифметическое действие, либо 0 (рандомное число): ")
+while znak != "q":
+    if znak == "+":
         print(plus(number1 = float(input("Введите 1 значение: ")),number2 = float(input("Введите 2 значение: "))))
 
-    elif do == "-":
+    elif znak == "-":
         print(minus(number1=float(input("Введите 1 значение: ")), number2=float(input("Введите 2 значение: "))))
 
-    elif do == "/":
+    elif znak == "/":
         print(div(number1=float(input("Введите 1 значение: ")), number2=float(input("Введите 2 значение: "))))
 
-    elif do == "*":
+    elif znak == "*":
         print(multi(number1=float(input("Введите 1 значение: ")), number2=float(input("Введите 2 значение: "))))
 
-    elif do == "^":
+    elif znak == "^":
         print(pow(number1=float(input("Введите 1 значение: ")), number2=float(input("Введите 2 значение: "))))
 
-    elif do == "module":
+    elif znak == "module":
         print(module(number1=float(input("Введите значение: "))))
 
-    elif do == "0":
+    elif znak == "0":
         print(random.randint(0, 1000))
         break
         
-    elif do == "f":
+    elif znak == "f":
         print(factorial(number1=int(input("Введите  значение: "))))
 
-    elif do == "arccos":
+    elif znak == "arccos":
         print(math.acos(arccos(number1=float(input("Введите  значение: ")))))
