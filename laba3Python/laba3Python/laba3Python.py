f5220cd00a7e005c40ff7ec3cbddd10063c380b1
khgpsk <86227559+khgpsk@users.noreply.github.com>
2021-06-22 10:37:50 +0300

    Update laba3Python.py

--- laba3Python/laba3Python/laba3Python.py
+++ laba3Python/laba3Python/laba3Python.py
@@ -38,19 +38,6 @@ elif znak == "module":
 elif znak == "0":
     print(random.randint(0, 1000))
 
-elif znak == "div":
-    number1 = float(input("Введите 1 значение: "))
-    number2 = float(input("Введите 2 значение: "))
-    if number2 != 0:
-        print("На 0 делить нельзя")
-    else:
-        print(number1//number2)
-
-elif znak == "mod":
-    number1 = float(input("Введите 1 значение: "))
-    number2 = float(input("Введите 2 значение: "))
-    print(number1 % number2)
-
 elif znak == "f":
     number1 = int(input("Введите значение: "))
     print(math.factorial(number1))
