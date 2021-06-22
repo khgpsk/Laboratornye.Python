132f5b7fc6d8ae84d9c3c6b6c113e169d5401e2b
khgpsk <86227559+khgpsk@users.noreply.github.com>
2021-06-22 09:43:58 +0300

    Update laba2Python.py

--- laba2Python/laba2Python/laba2Python.py
+++ laba2Python/laba2Python/laba2Python.py
@@ -1,8 +1,8 @@
 stroka = input("Введите слово: ")
 last_letter = stroka[:-1]
 stroka2 = ""
-for x in range(0, len(last_letter)):
-    if x != 2:
+for i in range(0, len(last_letter)):
+    if i != 2:
         stroka2 = stroka2 + last_letter[x]
 print(stroka2)
 if stroka.find("с") >= 0:
