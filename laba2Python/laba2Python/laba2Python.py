07b08a02c407387657cee5ded8b7eea6ab3717ab
khgpsk <86227559+khgpsk@users.noreply.github.com>
2021-06-22 09:46:56 +0300

    Update laba2Python.py

--- laba2Python/laba2Python/laba2Python.py
+++ laba2Python/laba2Python/laba2Python.py
@@ -3,7 +3,7 @@ last_letter = stroka[:-1]
 stroka2 = ""
 for i in range(0, len(last_letter)):
     if i != 2:
-        stroka2 = stroka2 + last_letter[x]
+        stroka2 = stroka2 + last_letter[i]
 print(stroka2)
 if stroka.find("с") >= 0:
     print("Есть с.")
